1a2c085be6e98600c47e921602a3415cf90ac7e9
tatsuya828828 <kkjy011894@gmail.com>
2020-06-22 22:11:03 +0900

    [finish]java-practice-1-11

---
+++ 11-6/Book.java
@@ -0,0 +1,13 @@
+
+public class Book extends TangibleAsset{
+	String isbn;
+
+	// コンストラクタ
+	public Book(String name, int price, String color, String isbn) {
+		super(name, price, isbn);
+		this.isbn = isbn;
+	}
+
+	// メソッド
+	public String getIsbn() {return this.isbn;}
+}
---
+++ 11-6/Computer.java
@@ -0,0 +1,13 @@
+
+public class Computer extends TangibleAsset {
+	String makerName;
+
+	// コンストラクタ
+	public Computer(String name, int price, String color, String makerName) {
+		super(name, price, color);
+		this.makerName = makerName;
+	}
+
+	// メソッド
+	public String getMakerName() {return this.makerName;}
+}
